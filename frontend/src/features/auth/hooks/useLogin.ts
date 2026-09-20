80294bd49f1a9d944012d55063862134d8fcd567
SeePui <ngseepui95@gmail.com>
2026-09-20 19:38:04 +0800

    feat: implement login page

---
+++ frontend/src/features/auth/hooks/useLogin.ts
@@ -0,0 +1,42 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { loginService } from "@/features/auth/services/auth.service";
+import { setToken } from "@/utils/token";
+import useAuthStore from "@/store/authStore";
+import { parseError } from "@/utils/errorHandler";
+
+import type { ParsedError } from "@/utils/errorHandler";
+import type { LoginPayload } from "../types/auth.types";
+import { toast } from "react-toastify";
+
+const useLogin = () => {
+  const navigate = useNavigate();
+  const setUser = useAuthStore((state) => state.setUser);
+
+  const { mutate, isPending, isError, error } = useMutation<
+    void,
+    ParsedError,
+    LoginPayload
+  >({
+    mutationFn: async (payload: LoginPayload) => {
+      try {
+        const result = await loginService(payload);
+        setToken(result.accessToken);
+        setUser(result.user);
+      } catch (rawError) {
+        throw parseError(rawError);
+      }
+    },
+    onSuccess: () => {
+      navigate("/supplier");
+      toast.success("Login successful!");
+    },
+    onError: (error) => {
+      toast.error(error.message)
+    },
+  });
+
+  return { login: mutate, isPending, isError, error };
+};
+
+export default useLogin;
