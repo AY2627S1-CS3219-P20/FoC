e61c8141e776d112f3eac7456782541d612429f1
ambertan <ambertjn@gmail.com>
2026-09-23 01:24:46 +0800

    feat: define expected post request input

--- supplier-service/src/schema/supplier.schema.ts
+++ supplier-service/src/schema/supplier.schema.ts
@@ -4,4 +4,9 @@ export const supplierSchema = z.object({
     page: z.coerce.number().int().positive(),
 });
 
+export const typeSchema = z.object({
+    type: z.string().toUpperCase() // ensure all types are saved in upper case
+});
+
 export type ViewSupplierInput = z.infer<typeof supplierSchema>;
+export type TypeInput = z.infer<typeof typeSchema>;
