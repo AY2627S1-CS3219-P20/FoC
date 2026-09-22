4f5823a6f374a77b14a353a7f8b033cfeb21afe6
ambertan <ambertjn@gmail.com>
2026-09-22 15:18:22 +0800

    feat: add api endpoint for fetching suppliers

--- supplier-service/src/controllers/supplier.controller.ts
+++ supplier-service/src/controllers/supplier.controller.ts
@@ -1,11 +1,24 @@
 import type { Request, Response } from "express";
+import { fetchSuppliers } from "../services/supplier.service.js";
+import { supplierSchema } from "../schema/supplier.schema.js";
+import { AppError } from "../errors/errors.js";
 
-export async function viewAllAvailableSuppliers(req: Request, res: Response) {
-    console.log("Fetching all suppliers...");
+export async function viewSuppliersInPage(req: Request, res: Response) {
+    // console.log("Fetching all suppliers...");
+
+    const request = supplierSchema.safeParse(req.query);
+    if (!request.success) {
+        // we need to make sure this block is not reached, i.e. the request body has a "page" field
+        request.error; // ZodError instance
+        throw new AppError("Parameters not input correctly", 400);
+    }
+    const suppliers = await fetchSuppliers(request.data.page);
+
     return res.status(200).json({
         success: true,
         data: {
             message: "All suppliers fetched successfully",
+            suppliers: suppliers,
         },
     });
 }
