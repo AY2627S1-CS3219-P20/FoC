2959bd9b334f83098476a2eb5ab69d5a05d18300
ambertan <ambertjn@gmail.com>
2026-09-23 18:48:57 +0800

    feat: add filter by supplier type

--- frontend/src/pages/SupplierPage.tsx
+++ frontend/src/pages/SupplierPage.tsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useQuery } from "@tanstack/react-query";
 import type { ParsedError } from "@/utils/errorHandler";
-import { viewSuppliersInPage, countActiveSuppliers } from "@/api/supplierApi";
-import type { Supplier } from "@/types/api.types";
+import { viewSuppliersInPage, countActiveSuppliers, getAllSupplierTypes } from "@/api/supplierApi";
+import type { Supplier, SupplierType } from "@/types/api.types";
 import SupplierCard from "@/features/supplier/components/SupplierCard";
 import { Card } from "@/components/ui/card";
 import { Pagination, PaginationContent, PaginationItem, PaginationPrevious, PaginationNext, PaginationLink } from "@/components/ui/pagination";
 import { Field } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
+import { Menubar, MenubarMenu, MenubarTrigger } from '@/components/ui/menubar';
+
 
 const SupplierPage = () => {
     const LIMIT: number = 15;
@@ -22,9 +24,13 @@ const SupplierPage = () => {
         console.log(searchString)
         setSearchString(event.target.value); 
     }
+    const handleFilterInput = (filter: string) => {
+        setTypeFilter(filter.toUpperCase()); 
+        console.log(typeFilter)
+    }
 
     const suppliersQuery = useQuery<Supplier[], ParsedError>({
-        queryKey: ["suppliers", searchString],
+        queryKey: ["suppliers", searchString, typeFilter],
         queryFn: () => viewSuppliersInPage(currentPage, searchString, typeFilter),
         refetchOnMount: "always",
     });
@@ -35,31 +41,60 @@ const SupplierPage = () => {
         refetchOnMount: "always",
     });
 
-    const isLoading = suppliersQuery.isLoading;
-    const isError = suppliersQuery.isError;
+    const supplierTypesQuery = useQuery<SupplierType[], ParsedError>({
+        queryKey: ["get-supplier-types"],
+        queryFn: () => getAllSupplierTypes(),
+        refetchOnMount: "always",
+    });
+
+    const suppliersIsLoading = suppliersQuery.isLoading;
+    const suppliersIsError = suppliersQuery.isError;
     const suppliers = suppliersQuery.data ?? [];
     const activeSupplierCount = countActiveSuppliersQuery.data ?? 0;
     const totalNumberOfPages = Math.ceil(activeSupplierCount / LIMIT);
 
+    const supplierTypesIsLoading = supplierTypesQuery.isLoading;
+    const supplierTypesIsError = supplierTypesQuery.isError;
+    const supplierTypes = supplierTypesQuery.data ?? [];
+
     return (
         <>
-            <div className="flex md:flex-row lg: flex-col items-start justify-between gap-4 px-5 md:px-10 py-5">
-                <h1 className="text-xl md:text-2xl font-bold">Suppliers</h1>
-                <Field className="md:w-full lg:w-[500px]">
-                    <Input
-                        id="input-search-key"
-                        type="text"
-                        placeholder="Search for a supplier"
-                        onChange={handleSearchKeyInput}
-                    />
-                </Field>
+            <div className="flex flex-col px-5 md:px-10 py-5 gap-4">
+                <div className="flex md:flex-row lg: flex-col items-start justify-between gap-4">
+                    <h1 className="text-xl md:text-2xl font-bold">Suppliers</h1>
+                    <Field className="md:w-full lg:w-[500px]">
+                        <Input
+                            id="input-search-key"
+                            type="text"
+                            placeholder="Search for a supplier"
+                            onChange={handleSearchKeyInput}
+                        />
+                    </Field>
+                </div>
+                {supplierTypesIsLoading && <p>Loading suppliers...</p>}
+                {supplierTypesIsError && <p>Failed to load suppliers: {supplierTypesQuery.error.message}</p>}
+
+                {!supplierTypesIsLoading && !supplierTypesIsError && suppliers.length === 0 && (
+                    <p className="text-sm text-muted-foreground">No suppliers yet.</p>
+                )}
+
+                <Menubar className="w-fit">
+                    <MenubarMenu>
+                        <MenubarTrigger onClick={() => handleFilterInput("ALL")}>ALL</MenubarTrigger>
+                    </MenubarMenu>
+                    {supplierTypes.map(type => (
+                        <MenubarMenu>
+                            <MenubarTrigger onClick={() => handleFilterInput(type.type)}>{type.type}</MenubarTrigger>
+                        </MenubarMenu>
+                    ))}
+                </Menubar>
             </div>
 
             <div className="px-5 md:px-10 pb-10">
-                {isLoading && <p>Loading suppliers...</p>}
-                {isError && <p>Failed to load suppliers: {suppliersQuery.error.message}</p>}
+                {suppliersIsLoading && <p>Loading suppliers...</p>}
+                {suppliersIsError && <p>Failed to load suppliers: {suppliersQuery.error.message}</p>}
 
-                {!isLoading && !isError && suppliers.length === 0 && (
+                {!suppliersIsLoading && !suppliersIsError && suppliers.length === 0 && (
                     <p className="text-sm text-muted-foreground">No suppliers yet.</p>
                 )}
 
