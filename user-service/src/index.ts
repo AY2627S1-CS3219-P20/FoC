5dc8a7416eaaa4409cfa71558e52c5f8c2b56137
SeePui <ngseepui95@gmail.com>
2026-09-26 16:25:20 +0800

    refactor: use config frontend url

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -10,7 +10,7 @@ import { AppError } from "./errors/errors.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: config.frontendUrl, credentials: true }));
 app.use(express.json());
 app.use(cookieParser(config.cookieSecret));
 
